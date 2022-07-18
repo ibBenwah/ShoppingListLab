d9bc662c7f724854ded909f1f54a37035f360bc8
ibBenwah <garret.c.benoit@gmail.com>
2022-07-18 18:29:55 -0400

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+//Int  of Arrays
+
+
+//int[] numbers = new int[5];
+//numbers[0] = 12;
+//numbers[1] = 3;
+//numbers[2] = 5;
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    Console.Write(numbers[i]);
+//}
+
+//foreach (int number in numbers)
+//{
+//    Console.Write(number + ", ");
+
+//}
+
+//Console.ReadKey();
+
+// Lists 
+//List<int> numbers = new List<int>();
+//numbers.Add(13);
+//numbers.Add(4);
+//numbers.Add(8);
+
+//for(int i = 0; i < numbers.Count; i++)          // Prints out numbers in array
+//{
+//    Console.Write(numbers[i] + ", ");
+//}
+//Console.WriteLine();
+
+//numbers.RemoveAt(1);                            // removes number at (n) position of array
+//for(int i = 0; i< numbers.Count; i++)
+//{
+//    Console.Write(numbers[i] + ", ");
+//}
+//Console.WriteLine();
+
+
+//       Multidimensional Arrya
+
+
+//int[,] grid = new int[8, 8];
+//grid[2, 3] = 3;
+//for(int x= 0; x < grid.GetLength(0); x++)
+//{
+//    for(int y= 0; y < grid.GetLength(1); y++)
+//    {
+//        Console.Write(grid[x, y] + ", ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+List<string> students = new List<string>() { "Michael", "Morgan", "Jacob", "Garret", "Brennan", "Jacob" };
+//for loop with .Count()
+for (int i = 0; i < students.Count(); i++)
+    {
+        Console.WriteLine(students[i]);
+    }
+
+//  .IndexOf
+int index = students.IndexOf("Jacob");
+Console.WriteLine(index);
---
+++ ShoppingListLab/Program.cs
@@ -0,0 +1,56 @@
+﻿//      Shopping List Lab
+
+
+List<string> items = new List<string>() { "Milk", "Eggs", "Poptarts", "Pop", "Beer", "Chicken nuggets", "Cream Cheese", "Waffles" };
+List<string> prices = new List<string>() { "1.99", "1.50", "3.50", "4.75", "10.99", "11.99", "3.49", "5.99" };
+List<string> groceryList = new List<string>();
+List<string> groceryPrices = new List<string>();
+bool isContinue = true;
+do
+{
+    do
+    {
+        Console.Write("What would you like to add to the cart? ");
+        string userInput = Console.ReadLine();
+        if (items.Contains(userInput))
+        {
+            int index = items.IndexOf(userInput);
+            Console.Write(items[index] + " ");
+            groceryList.Add(items[index]);
+            Console.WriteLine(prices[index] + " ");
+            groceryPrices.Add(prices[index]);
+        }
+        else
+        {
+            Console.Write("Enter a valid item.");
+        }
+
+        Console.Write("would you like to keep shopping? (yes or no) ");
+        if (Console.ReadLine() == "yes")
+        {
+            
+        }
+        else
+        {
+            Console.Clear();
+            break;
+        }
+
+    } while (isContinue);
+
+    Console.WriteLine("you ordered: ");
+    
+    for (int i = 0; i < groceryList.Count ; i++)
+    {
+        
+        Console.WriteLine(groceryPrices[i] +" ");
+    }
+    for (int j = 0; j < groceryPrices.Count; j++)
+    {
+
+        Console.WriteLine(groceryList[j] + " ");
+    }
+
+} while (isContinue = false);
+Console.WriteLine("Thank You for shopping!");
+
